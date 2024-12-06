1932c0d00da9ea94d98e7adb02c86cce60f3ff8e
Tom Dolan <tddzyx@gmail.com>
2024-12-05 22:01:03 -0500

    allow opening acordion when clicking anywhere on it

--- web/app/database/page.tsx
+++ web/app/database/page.tsx
@@ -5,11 +5,12 @@ import { useState } from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import LaunchIcon from '@mui/icons-material/Launch';
-import router, { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Info() {
   const [expanded, setExpanded] = useState<number>(-1);
   const router = useRouter();
+  const toggleOpen = (index: number) => setExpanded(expanded === index ? -1 : index);
   return (
     <Box justifyItems="center">
       <Stack direction="column" alignItems="center" width="60rem" spacing={2}>
@@ -18,17 +19,17 @@ export default function Info() {
         <Divider flexItem />
         <Box>
           {data.map((entry, index) => (
-            <Accordion key={index} expanded={expanded === index}>
+            <Accordion key={index} expanded={expanded === index} onChange={() =>toggleOpen(index)}>
               <AccordionSummary>
                 <Stack flex={1} direction="row" justifyContent="space-between">
                   <Box>
                     <Typography variant="h6" noWrap width="45rem">
-                      "{entry.title}"
+                      {entry.title}
                     </Typography>
                     <Typography>By: {entry.author}</Typography>
                   </Box>
                   <Box>
-                    <IconButton color="primary" onClick={() => setExpanded(expanded === index ? -1 : index)} sx={{ marginRight: 2 }}>
+                    <IconButton color="primary" onClick={() => toggleOpen(index)} sx={{ marginRight: 2 }}>
                       {expanded === index ? <InfoIcon /> : <InfoOutlinedIcon />}
                     </IconButton>
                     <Button variant="outlined" endIcon={<LaunchIcon />} onClick={() => router.push(entry.link)}>visit</Button>
