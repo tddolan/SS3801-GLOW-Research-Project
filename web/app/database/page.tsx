73c47a7ce8b4c54d6d1121e1c22962df68fa5511
Tom Dolan <tddzyx@gmail.com>
2024-12-06 00:04:16 -0500

    added pagination

--- web/app/database/page.tsx
+++ web/app/database/page.tsx
@@ -1,25 +1,28 @@
 "use client";
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, Divider, IconButton, Stack, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, Divider, IconButton, Pagination, Stack, Typography } from "@mui/material";
 import { data } from "./database-entries";
 import { useState } from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import LaunchIcon from '@mui/icons-material/Launch';
-import { useRouter } from "next/navigation";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 export default function Info() {
   const [expanded, setExpanded] = useState<number>(-1);
-  const router = useRouter();
+  const [page, setPage] = useState<number>(1);
   const toggleOpen = (index: number) => setExpanded(expanded === index ? -1 : index);
+  const pagedData = data.slice((page-1)*10, Math.min(page*10, data.length));
   return (
-    <Box justifyItems="center">
+    <Box justifyItems="center" padding={3}>
       <Stack direction="column" alignItems="center" width="60rem" spacing={2}>
         <Typography variant="h3">GLOW Database</Typography>
-        <Typography>This page contains many useful sources on Great Lakes offshore wind. Click the <InfoOutlinedIcon color="primary" fontSize="small"/> icon for a description of the source.</Typography>
+        <Typography>
+          This page contains many useful sources on Great Lakes offshore wind. Click the <InfoOutlinedIcon color="primary" fontSize="small" /> icon for a
+          description of the source.
+        </Typography>
         <Divider flexItem />
         <Box>
-          {data.map((entry, index) => (
-            <Accordion key={index} expanded={expanded === index} onChange={() =>toggleOpen(index)}>
+          {pagedData.map((entry, index) => (
+            <Accordion key={index} expanded={expanded === index} onChange={() => toggleOpen(index)}>
               <AccordionSummary>
                 <Stack flex={1} direction="row" justifyContent="space-between">
                   <Box>
@@ -32,7 +35,16 @@ export default function Info() {
                     <IconButton color="primary" onClick={() => toggleOpen(index)} sx={{ marginRight: 2 }}>
                       {expanded === index ? <InfoIcon /> : <InfoOutlinedIcon />}
                     </IconButton>
-                    <Button variant="outlined" endIcon={<LaunchIcon />} onClick={() => router.push(entry.link)}>visit</Button>
+                    <Button
+                      variant="outlined"
+                      endIcon={<LaunchIcon />}
+                      onClick={(event) => {
+                        event.stopPropagation(); // prevents clicks from triggering parent's onClick
+                        window.open(entry.link);
+                      }}
+                    >
+                      visit
+                    </Button>
                   </Box>
                 </Stack>
               </AccordionSummary>
@@ -45,6 +57,7 @@ export default function Info() {
             </Accordion>
           ))}
         </Box>
+        <Pagination count={Math.ceil(data.length/10)} page={page} onChange={(event, value) => setPage(value)}/>
       </Stack>
     </Box>
   );
